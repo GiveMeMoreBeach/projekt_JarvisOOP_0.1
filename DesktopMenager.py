26f734a85d1f63b533d918cecb2d5b53282e96c7
GiveMeMoreBeach <androssa702@gmail.com>
2026-04-13 18:57:04 +0200

    test

---
+++ DesktopMenager.py
@@ -0,0 +1,61 @@
+import os
+from send2trash import send2trash
+from Talk import Talk
+
+class DesktopManager(object):
+    def __init__(self):
+        self.pulpit = os.path.join(os.path.expanduser("~"),"Desktop")
+        self.pliki = os.path.join(os.path.expanduser("~"),"Pliki")
+        self.talk = Talk()
+
+    def ile_plikow(self):
+        pliki = os.listdir(self.pulpit)
+        self.talk.talk(f"Na Pulpicie jest {len(pliki)} plikow")
+        return pliki
+
+    def stworz_folder(self,nazwaFolderu):
+        sciezka = os.path.join(self.pulpit,nazwaFolderu)
+        if not os.path.exists(sciezka):
+            os.mkdir(sciezka)
+            self.talk.talk(f"Folder {nazwaFolderu} zostal stworzony")
+        else:
+            self.talk.talk(f"Folder {nazwaFolderu} już istnieje")
+
+    def stworz_plik(self,nazwa):
+        sciezka = os.path.join(self.pulpit,nazwa)
+        if not os.path.exists(sciezka):
+            open(sciezka,"w").close
+            self.talk.talk("Plik zostal stworzony")
+        else:
+            self.talk.talk("[Plik  już istnieje")
+    def stworz_plik_z_zawartoscia(self,nazwa,tresc):
+        sciezka = os.path.join(self.pulpit,nazwa)
+        if not os.path.exists(sciezka):
+            with open(sciezka,"w") as f:
+                f.write(tresc)
+            self.talk.talk("Plik zostal stworzony")
+        else:
+            self.talk.talk("Plik juz istnieje")
+
+    def usun_plik_do_kosza(self,nazwa):
+        sciezka = os.path.join(self.pulpit,nazwa)
+        if not os.path.exists(sciezka):
+            self.talk.talk(f"Folder {nazwa} nie istnieje")
+        else:
+            send2trash(sciezka)
+            self.talk.talk(f"Folder {nazwa} zostal usunięty")
+
+    def usun_plik_klucz(self,klucz):
+        self.pliki = self.ile_plikow()
+        licznik = 0
+
+        for plik in self.pliki:
+            if klucz.lower() in plik.lower():
+                sciezka = os.path.join(self.pulpit,plik)
+                send2trash(sciezka)
+                print(f"plik o nazwie {plik} zostal usuniety ")
+                licznik += 1
+        if licznik > 0:
+            self.talk.talk(f"Zdołałem usunąć : {licznik} plik")
+        else:
+            self.talk.talk("Nie usunalem zadnego pliku")
---
+++ main.py
@@ -0,0 +1,7 @@
+from DesktopMenager import DesktopManager
+
+
+dm = DesktopManager()
+
+dm.stworz_plik("Test")
+dm.stworz_plik_z_zawartoscia("TestX","HELLO IM JARVIS!")
